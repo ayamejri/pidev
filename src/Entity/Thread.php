9dbc4ad525d3611d941d49c763d93ff1b50f3703
oussama hannachi <100031142+yamako2002@users.noreply.github.com>
2024-03-02 14:58:32 +0100

    first commit

---
+++ src/Entity/Thread.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ThreadRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ThreadRepository::class)]
+class Thread
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $topic = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $createdat = null;
+
+    #[ORM\OneToMany(mappedBy: 'thread', targetEntity: Post::class)]
+    private Collection $relation;
+
+    public function __construct()
+    {
+        $this->relation = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): static
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getCreatedat(): ?\DateTimeInterface
+    {
+        return $this->createdat;
+    }
+
+    public function setCreatedat(\DateTimeInterface $createdat): static
+    {
+        $this->createdat = $createdat;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, post>
+     */
+    
+    public function getRelation(): Collection
+    {
+        return $this->relation;
+    }
+
+    public function addRelation(post $relation): static
+    {
+        if (!$this->relation->contains($relation)) {
+            $this->relation->add($relation);
+            $relation->setThread($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRelation(post $relation): static
+    {
+        if ($this->relation->removeElement($relation)) {
+            // set the owning side to null (unless already changed)
+            if ($relation->getThread() === $this) {
+                $relation->setThread(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Form/PostType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType; 
+use App\Entity\Thread; 
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('description')
+            ->add('createdAt', DateTimeType::class, [ 
+                'widget' => 'choice', // Render as a single input field
+                'html5' => false, // Allow non-HTML5 browsers to fallback to a text input
+                'format' => 'yyyy-MM-dd HH:mm:ss', // Date and time format
+                // You can add more options like 'attr' to specify additional attributes for the input field if needed
+            ])
+            ->add('thread', EntityType::class, [ 
+                'class' => Thread::class, 
+                'choice_label' => 'title', 
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
